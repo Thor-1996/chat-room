3902abf18e12eadf0be26a99e7825effa86609a9
Thor <15073245605@163.com>
2020-10-15 15:18:04 +0800

    改为域名

--- serve/index.js
+++ serve/index.js
@@ -1,51 +1,50 @@
-/* let ws = require("nodejs-websocket");
-let onlineCount = 0;
-const COUNT = "COUNT";
-const MSG = "MSG";
+// let ws = require("nodejs-websocket");
+// let onlineCount = 0;
+// const COUNT = "COUNT";
+// const MSG = "MSG";
 
-const server = ws
-  .createServer(function (conn) {
-    onlineCount++;
+// const server = ws
+//   .createServer(function (conn) {
+//     onlineCount++;
 
-    boardcast({
-      type: COUNT,
-      count: onlineCount,
-      key: conn.key,
-    });
+//     boardcast({
+//       type: COUNT,
+//       count: onlineCount,
+//       key: conn.key,
+//     });
 
-    conn.on("text", function (str) {
-      const time = +new Date();
+//     conn.on("text", function (str) {
+//       const time = +new Date();
 
-      boardcast({
-        type: MSG,
-        key: conn.key,
-        time,
-        msg: str,
-      });
-    });
+//       boardcast({
+//         type: MSG,
+//         key: conn.key,
+//         time,
+//         msg: str,
+//       });
+//     });
 
-    conn.on("close", function (code, reason) {
-      onlineCount--;
+//     conn.on("close", function (code, reason) {
+//       onlineCount--;
 
-      boardcast({
-        type: COUNT,
-        count: onlineCount,
-        key: conn.key,
-      });
-    });
+//       boardcast({
+//         type: COUNT,
+//         count: onlineCount,
+//         key: conn.key,
+//       });
+//     });
 
-    conn.on("error", function (code, reason) {
-      console.log("异常关闭");
-    });
-  })
-  .listen(8001);
+//     conn.on("error", function (code, reason) {
+//       console.log("异常关闭");
+//     });
+//   })
+//   .listen(8001);
 
-function boardcast(obj) {
-  server.connections.forEach(function (conn) {
-    conn.send(JSON.stringify(obj));
-  });
-}
-*/
+// function boardcast(obj) {
+//   server.connections.forEach(function (conn) {
+//     conn.send(JSON.stringify(obj));
+//   });
+// }
 
 var https = require("https");
 var ws = require("ws");
