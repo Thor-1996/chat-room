a42d52148f758290636cd49d556c983c2a8329bc
Thor <15073245605@163.com>
2020-10-15 17:25:36 +0800

    wss完成

--- serve/index.js
+++ serve/index.js
@@ -1,72 +1,60 @@
-// let ws = require("nodejs-websocket");
-// let onlineCount = 0;
-// const COUNT = "COUNT";
-// const MSG = "MSG";
+let https = require("https");
+let ws = require("ws");
+let fs = require("fs");
+let keypath = process.cwd() + "\\2_chat.webliker.cn.key";
+let certpath = process.cwd() + "\\1_chat.webliker.cn_bundle.crt";
 
-// const server = ws
-//   .createServer(function (conn) {
-//     onlineCount++;
+let onlineCount = 0;
+const COUNT = "COUNT";
+const MSG = "MSG";
 
-//     boardcast({
-//       type: COUNT,
-//       count: onlineCount,
-//       key: conn.key,
-//     });
-
-//     conn.on("text", function (str) {
-//       const time = +new Date();
-
-//       boardcast({
-//         type: MSG,
-//         key: conn.key,
-//         time,
-//         msg: str,
-//       });
-//     });
-
-//     conn.on("close", function (code, reason) {
-//       onlineCount--;
-
-//       boardcast({
-//         type: COUNT,
-//         count: onlineCount,
-//         key: conn.key,
-//       });
-//     });
-
-//     conn.on("error", function (code, reason) {
-//       console.log("异常关闭");
-//     });
-//   })
-//   .listen(8001);
-
-// function boardcast(obj) {
-//   server.connections.forEach(function (conn) {
-//     conn.send(JSON.stringify(obj));
-//   });
-// }
-
-var https = require("https");
-var ws = require("ws");
-var fs = require("fs");
-var keypath = process.cwd() + "\\2_chat.webliker.cn.key";
-var certpath = process.cwd() + "\\1_chat.webliker.cn_bundle.crt";
-
-var options = {
+const options = {
   key: fs.readFileSync(keypath),
   cert: fs.readFileSync(certpath),
 };
 
-var server = https
+const server = https
   .createServer(options, function (req, res) {
     res.writeHead(403);
     res.end("This is a  WebSockets server!\n");
   })
   .listen(8001);
 
-var wss = new ws.Server({ server: server });
-wss.on("connection", function (wsConnect) {
-  wsConnect.on("message", function (message) {
-    console.log(message);
+const wss = new ws.Server({ server: server });
+wss.on("connection", function (conn) {
+  console.log(conn);
+  onlineCount++;
+
+  boardcast({
+    type: COUNT,
+    count: onlineCount,
+    key: conn.key,
+  });
+
+  conn.on("message", function (message) {
+    const time = +new Date();
+
+    boardcast({
+      type: MSG,
+      key: conn.key,
+      time,
+      msg: message,
+    });
+  });
+
+  conn.on("close", function () {
+    onlineCount--;
+
+    boardcast({
+      type: COUNT,
+      count: onlineCount,
+      key: conn.key,
+    });
   });
 });
+
+function boardcast(obj) {
+  wss.clients.forEach(function (client) {
+    client.send(JSON.stringify(obj));
+  });
+}
